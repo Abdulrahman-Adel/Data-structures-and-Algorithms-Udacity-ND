a06c5878372cb7005f956d4a584745f8869b5aab
Abdulrahman-Adel <57441828+Abdulrahman-Adel@users.noreply.github.com>
2020-11-14 12:30:40 +0200

    Add files via upload

---
+++ Recursion/Keypad Combinations.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 17:39:55 2020
+
+@author: Abdelrahman
+"""
+def get_characters(num):
+    if num == 2:
+        return "abc"
+    elif num == 3:
+        return "def"
+    elif num == 4:
+        return "ghi"
+    elif num == 5:
+        return "jkl"
+    elif num == 6:
+        return "mno"
+    elif num == 7:
+        return "pqrs"
+    elif num == 8:
+        return "tuv"
+    elif num == 9:
+        return "wxyz"
+    else:
+        return ""
+
+
+def keypad(num):
+    if num == 0:
+        return [""]
+
+    elif num / 10 < 1:
+        return [element for element in get_characters(num)]
+
+    else:
+        input_level = get_characters(num % 10)
+        input_prev = keypad(num // 10)
+
+        result = []
+
+        for i_a in input_level:
+            for i_b in input_prev:
+                result.append(i_b + i_a)
+
+        return result
+    
+def test_keypad(input, expected_output):
+    if sorted(keypad(input)) == expected_output:
+        print("Yay. We got it right.")
+    else:
+        print("Oops! That was incorrect.")
+        
+input = 32
+expected_output = sorted(["da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"])
+test_keypad(input, expected_output)
+
+input = 354
+expected_output = sorted(["djg", "ejg", "fjg", "dkg", "ekg", "fkg", "dlg", "elg", "flg", "djh", "ejh", "fjh", "dkh", "ekh", "fkh", "dlh", "elh", "flh", "dji", "eji", "fji", "dki", "eki", "fki", "dli", "eli", "fli"])
+test_keypad(input, expected_output)        
