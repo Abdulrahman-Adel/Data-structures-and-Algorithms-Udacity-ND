83789e9d93a20a36648d27711c4725972ba68c4c
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2026-05-10 16:45:19 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,14 +1,28 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowles = 'aeiou'
-        vowlesIndecies = []
-        vowlesInS = []
-        sList = [char for char in s]
-        for i in range(len(s)):
-            if s[i].lower() in vowles:
-                vowlesIndecies.append(i)
-                vowlesInS.append(s[i])
+        # vowles = 'aeiou'
+        # vowlesIndecies = []
+        # vowlesInS = []
+        # sList = [char for char in s]
+        # for i in range(len(s)):
+        #     if s[i].lower() in vowles:
+        #         vowlesIndecies.append(i)
+        #         vowlesInS.append(s[i])
 
-        for j in range(len(vowlesIndecies)):
-            sList[vowlesIndecies[j]] = vowlesInS[len(vowlesIndecies)-1-j]
-        return ''.join(sList)
+        # for j in range(len(vowlesIndecies)):
+        #     sList[vowlesIndecies[j]] = vowlesInS[len(vowlesIndecies)-1-j]
+        # return ''.join(sList)
+        vowles = 'aeiou'
+
+        stack = []
+        for char in s:
+            if char.lower() in vowles:
+                stack.append(char)
+        
+        finalword = ''
+        for char in s:
+            if char.lower() in vowles:
+                finalword += stack.pop()
+            else:
+                finalword += char
+        return finalword
