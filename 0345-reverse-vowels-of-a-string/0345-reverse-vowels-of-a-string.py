c9531ddc1931857bdc827f77b564a4902176ee4b
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2026-05-10 17:04:13 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -12,17 +12,47 @@ class Solution:
         # for j in range(len(vowlesIndecies)):
         #     sList[vowlesIndecies[j]] = vowlesInS[len(vowlesIndecies)-1-j]
         # return ''.join(sList)
-        vowles = 'aeiou'
+        # vowles = 'aeiou'
 
-        stack = []
-        for char in s:
-            if char.lower() in vowles:
-                stack.append(char)
+        # stack = []
+        # for char in s:
+        #     if char.lower() in vowles:
+        #         stack.append(char)
         
-        finalword = ''
-        for char in s:
-            if char.lower() in vowles:
-                finalword += stack.pop()
-            else:
-                finalword += char
-        return finalword
+        # finalword = ''
+        # for char in s:
+        #     if char.lower() in vowles:
+        #         finalword += stack.pop()
+        #     else:
+        #         finalword += char
+        # return finalword
+        #########  Third Solution ################
+        ########### Two pointers ###############
+        """
+        I c e C r e A m
+        ^             ^   → both vowels (I, m? no) → left=I, right skips m,A
+        I c e C r e A m
+        ^           ^     → swap I and A
+        A c e C r e I m
+        ^       ^       → left skips c, right skips I... wait I is vowel
+        A c e C r e I m
+            ^     ^       → swap e and I  ... and so on
+        """
+        vowles = 'aeiou'
+        left, right = 0, len(s) - 1
+        sList = list(s)
+        while left < right:
+            
+            while left < right and s[right].lower() not in vowles:
+                right -= 1
+            
+            while left < right and s[left].lower() not in vowles:
+                left += 1
+
+            sList[left], sList[right] = sList[right], sList[left]
+            left += 1 
+            right -= 1
+        return ''.join(sList)         
+
+
+
