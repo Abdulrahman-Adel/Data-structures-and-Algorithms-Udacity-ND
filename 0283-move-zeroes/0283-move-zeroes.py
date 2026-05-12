a3f59bab9391839caf62f92a6968fec2a11f2d76
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2026-05-12 22:02:21 +0400

    Time: 12 ms (13.43%), Space: 20.3 MB (89.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,14 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # if len(nums)==1:
+        #     return nums
+        # zeroPointer = 0
+        # for i in range(len(nums)):
+        #     if nums[i]==0:
+        #         zeroPointer = i
+        #         for j in range(i, len(nums)):
+        #             if nums[j] != 0:
+        #                 nums[zeroPointer], nums[j] = nums[j], nums[zeroPointer]
+        #                 zeroPointer += 1
+        # return nums
         if len(nums)==1:
             return nums
         zeroPointer = 0
+        NonZeroPointer = 0
         for i in range(len(nums)):
             if nums[i]==0:
                 zeroPointer = i
-                for j in range(i, len(nums)):
-                    if nums[j] != 0:
-                        nums[zeroPointer], nums[j] = nums[j], nums[zeroPointer]
-                        zeroPointer += 1
+                break
+        
+        for i in range(len(nums)):
+            if nums[i] !=0:
+                NonZeroPointer = i
+                if zeroPointer <= NonZeroPointer:
+                    nums[zeroPointer], nums[NonZeroPointer] = nums[NonZeroPointer], nums[zeroPointer]
+                    zeroPointer += 1
         return nums
