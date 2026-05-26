eb3bc4f5b0838ef1a7eea0e9f7391e8169586164
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2026-05-26 10:42:01 +0400

    Time: 10 ms (40.56%), Space: 19.3 MB (88%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,21 +1,26 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        hashmap = {}
+        # hashmap = {}
 
-        for i in range(len(nums1)):
-            hashmap[nums1[i]] = (1, True)
+        # for i in range(len(nums1)):
+        #     hashmap[nums1[i]] = (1, True)
 
-        for i in range(len(nums2)):
-            if hashmap.get(nums2[i]) and hashmap[nums2[i]][0] != 2:
-                hashmap[nums2[i]] = (0, False)
-            else:
-                hashmap[nums2[i]] = (2, True)
+        # for i in range(len(nums2)):
+        #     if hashmap.get(nums2[i]) and hashmap[nums2[i]][0] != 2:
+        #         hashmap[nums2[i]] = (0, False)
+        #     else:
+        #         hashmap[nums2[i]] = (2, True)
         
 
-        answer = [[], []]
-        for key, value in hashmap.items():
-            if value[1] ==True:
-                answer[value[0] - 1].append(key)
-        return answer
+        # answer = [[], []]
+        # for key, value in hashmap.items():
+        #     if value[1] ==True:
+        #         answer[value[0] - 1].append(key)
+        # return answer
+        # A much simpler solution
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        return [list(set1 - set2), list(set2 - set1)]
         
         
