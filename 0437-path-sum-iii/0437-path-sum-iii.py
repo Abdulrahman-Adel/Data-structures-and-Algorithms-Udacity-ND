0d787b3fd98a5f5ec0c9c3e8ff949ad6c7ceb197
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2026-06-15 13:39:42 +0400

    Time: 4 ms (74.75%), Space: 20.2 MB (55.3%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -9,27 +9,49 @@ from pprint import pprint
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         # I think the most difficult part with this problem is that the path doesn't have to start from the root
-        # so you have to double run you algorithm treating each node as root to compute the paths
+        # so you have to double run your algorithm treating each node as root to compute the paths
         # so run a dfs and while on current node run another one but with this one caclute the sum so far
+        
+        # this algorithm runs on O(nlogn) or O(n^2)
+        # self.total = 0
+        # def helper(node, sumSoFar):
+        #     if not node:
+        #         return 
+        #     sumSoFar += node.val
+        #     if sumSoFar == targetSum:
+        #         self.total += 1
+        #     helper(node.right, sumSoFar)
+        #     helper(node.left, sumSoFar)
+        
+        # def dfs(node):
+        #     if not node:
+        #         return
+        #     helper(node, 0)
+        #     dfs(node.right)
+        #     dfs(node.left)
+        
+        # dfs(root)
+        # return self.total
 
-        self.total = 0
-        def helper(node, sumSoFar):
+        # 
+        def dfs(node, current_sum):
             if not node:
-                return 
-            sumSoFar += node.val
-            if sumSoFar == targetSum:
-                self.total += 1
-            helper(node.right, sumSoFar)
-            helper(node.left, sumSoFar)
-        
-        def dfs(node):
-            if not node:
-                return
-            helper(node, 0)
-            dfs(node.right)
-            dfs(node.left)
-        
-        dfs(root)
-        return self.total
+                return 0
+
+            current_sum += node.val
+            count = prefix_sums.get(current_sum - targetSum, 0)
+
+            prefix_sums[current_sum] = prefix_sums.get(current_sum, 0) + 1
+            count += dfs(node.left, current_sum)
+            count += dfs(node.right, current_sum)
+            prefix_sums[current_sum] -= 1
+
+            return count
+
+        prefix_sums = {0: 1}
+        return dfs(root, 0)
+            
+
+
         
 
