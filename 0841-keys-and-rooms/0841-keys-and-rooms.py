73cc72d9e2764f4ad8d711db97954480aacf48ae
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2026-06-25 19:32:29 +0400

    Time: 19 ms (5.13%), Space: 20.4 MB (19.66%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        # the idea of that problem is that when you visit a node you're given
+        # access to (x1, x2, ..) other nodes which can be possible paths for you
+        # so you don't have to go one by one
+
+
+        visited = [0]
+
+        def traverseNode(node):
+            for item in node:
+                if item not in visited:
+                    visited.append(item)
+                    traverseNode(rooms[item])
+            
+            return
+        
+        traverseNode(rooms[0])
+        return len(visited)==len(rooms)
