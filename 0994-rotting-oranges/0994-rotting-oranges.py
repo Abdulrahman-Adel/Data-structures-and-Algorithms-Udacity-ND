6469e11afe9f47f503cdfc2b58593bf00cc6354e
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2026-06-28 14:09:20 +0400

    Time: 14 ms (6.12%), Space: 20 MB (12.79%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,132 +5,133 @@ class Solution:
         # initially maybe we should start by fining out all the positions
         # of rotten oranges
 
-        # directions = [
-        #     (1, 0),   # down
-        #     (-1, 0),  # up
-        #     (0, 1),   # right
-        #     (0, -1)   # left
-        # ]
+        directions = [
+            (1, 0),   # down
+            (-1, 0),  # up
+            (0, 1),   # right
+            (0, -1)   # left
+        ]
 
-        # rotten_oranges = []
-        # fresh_oranges = []
-        # visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]      
-        # def dfs(r, c):
-        #     nonlocal rotten_oranges, fresh_oranges
-        #     # boundaries
-        #     if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
-        #         return
+        rotten_oranges = []
+        fresh_oranges = []
+        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]      
+        def dfs(r, c):
+            nonlocal rotten_oranges, fresh_oranges
+            # boundaries
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+                return
 
-        #     # check if visited
-        #     if visited[r][c]:
-        #         return 
+            # check if visited
+            if visited[r][c]:
+                return 
             
-        #     # mark as visited
-        #     visited[r][c] = True
+            # mark as visited
+            visited[r][c] = True
 
-        #     if grid[r][c]==2:
-        #         rotten_oranges.append([r, c])
-        #     elif grid[r][c]==1:
-        #         fresh_oranges.append([r, c])
+            if grid[r][c]==2:
+                rotten_oranges.append([r, c])
+            elif grid[r][c]==1:
+                fresh_oranges.append([r, c])
             
-        #     for dr, dc in directions:
-        #         dfs(r + dr, c + dc)
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
 
-        # dfs(0, 0)
+        dfs(0, 0)
 
-        # if not fresh_oranges:
-        #     return 0
+        if not fresh_oranges:
+            return 0
         
-        # from collections import deque
-        # visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]    
-        # def validPosition(dr, dc):
-        #     if dr < 0 or dr >= len(grid) or dc < 0 or dc >= len(grid[0]):
-        #         return False
+        from collections import deque
+        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]    
+        def validPosition(dr, dc):
+            if dr < 0 or dr >= len(grid) or dc < 0 or dc >= len(grid[0]):
+                return False
             
-        #     return True if grid[dr][dc]==1 and visited[dr][dc]==False else False
+            return True if grid[dr][dc]==1 and visited[dr][dc]==False else False
 
-        # got_rotten = []
-        # def bfs(r, c):
-        #     nonlocal got_rotten
-        #     queue = deque()
+        got_rotten = []
+        def bfs(rotten):
+            nonlocal got_rotten
+            queue = deque()
             
-        #     queue.append((r, c))
-        #     visited[r][c]=True
-        #     minutes = 0
-        #     while queue:
-        #         remove = len(queue)
-        #         for item in range(remove):
-        #             r, c = queue.popleft()
-        #             for dr, dc in directions:
-        #                 if validPosition(r+dr, c+dc):
-        #                     visited[r+dr][c+dc]=True
-        #                     got_rotten.append([r+dr, c+dc])
-        #                     queue.append([r+dr, c+dc])
-        #         if not queue:
-        #             break
-        #         minutes +=1
+            for r, c in rotten:
+                queue.append([r, c])
+            visited[r][c]=True
+            minutes = 0
+            while queue:
+                remove = len(queue)
+                for item in range(remove):
+                    r, c = queue.popleft()
+                    for dr, dc in directions:
+                        if validPosition(r+dr, c+dc):
+                            visited[r+dr][c+dc]=True
+                            got_rotten.append([r+dr, c+dc])
+                            queue.append([r+dr, c+dc])
+                if not queue:
+                    break
+                minutes +=1
             
-        #     return minutes
+            return minutes
 
-        # if rotten_oranges:
-        #     result = bfs(rotten_oranges[0][0], rotten_oranges[0][1])
-        # else:
-        #     return -1
+        if rotten_oranges:
+            result = bfs(rotten_oranges)
+        else:
+            return -1
          
-        # if len(got_rotten)!=len(fresh_oranges):
-        #     return -1
-        # return result
+        if len(got_rotten)!=len(fresh_oranges):
+            return -1
+        return result
 
         # this attempt was fine but the only thing that was missing is that why don't you just start the queue of the bfs
         # with the rotten places (very simple) and you will already process all at the same time with each minute 
-        from collections import deque
+        # from collections import deque
 
-        rows = len(grid)
-        cols = len(grid[0])
+        # rows = len(grid)
+        # cols = len(grid[0])
 
-        directions = [
-            (1,0),
-            (-1,0),
-            (0,1),
-            (0,-1)
-        ]
+        # directions = [
+        #     (1,0),
+        #     (-1,0),
+        #     (0,1),
+        #     (0,-1)
+        # ]
 
-        queue = deque()
-        fresh = 0
+        # queue = deque()
+        # fresh = 0
 
-        # collect initial rotten oranges
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    queue.append((r,c))
-                elif grid[r][c] == 1:
-                    fresh += 1
+        # # collect initial rotten oranges
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if grid[r][c] == 2:
+        #             queue.append((r,c))
+        #         elif grid[r][c] == 1:
+        #             fresh += 1
 
-        minutes = 0
+        # minutes = 0
 
-        while queue and fresh > 0:
+        # while queue and fresh > 0:
 
-            # process current minute
-            for _ in range(len(queue)):
+        #     # process current minute
+        #     for _ in range(len(queue)):
 
-                r, c = queue.popleft()
+        #         r, c = queue.popleft()
 
-                for dr, dc in directions:
-                    nr = r + dr
-                    nc = c + dc
+        #         for dr, dc in directions:
+        #             nr = r + dr
+        #             nc = c + dc
 
-                    if (
-                        0 <= nr < rows and
-                        0 <= nc < cols and
-                        grid[nr][nc] == 1
-                    ):
-                        grid[nr][nc] = 2
-                        fresh -= 1
-                        queue.append((nr,nc))
+        #             if (
+        #                 0 <= nr < rows and
+        #                 0 <= nc < cols and
+        #                 grid[nr][nc] == 1
+        #             ):
+        #                 grid[nr][nc] = 2
+        #                 fresh -= 1
+        #                 queue.append((nr,nc))
 
-            minutes += 1
+        #     minutes += 1
 
-        return minutes if fresh == 0 else -1
+        # return minutes if fresh == 0 else -1
         
                     
 
